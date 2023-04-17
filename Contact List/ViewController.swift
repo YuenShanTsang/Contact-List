120fadf51bad84c3f223438d7328218d4d30713a
YuenShanTsang <A00270323@mycambrian.ca>
2023-04-16 20:35:06 -0400

    Initial Commit

---
+++ Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact List
+//
+//  Created by Yuen Shan Tsang on 16/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
